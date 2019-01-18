098f4d3ec73d3b840244d56ea9a3dcf68980fb52
Ravik <ravik@in-Ravik.local>
2019-01-18 22:39:14 +0530

    Ravi | Add docker ignore and fix travis push config

--- build_docker.sh
+++ build_docker.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-main() {
+commit_number=$(git rev-list HEAD --count)
+
+create_docker_image() {
   echo "Creating docker image"
-  commit_number=$(git rev-list HEAD --count)
   docker build . -t ilearnttoday/bloguserinterface:v${commit_number}
   echo "Created docker image"
+}
+
+push_docker_image() {
+  echo "Pushing docker image"
+  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD ;
+  docker push ilearnttoday/bloguserinterface:v$(commit_number) ;
+  echo "Pushed docker image"
+}
+
+main() {
+  create_docker_image
+  push_docker_image
 }
 
 main
